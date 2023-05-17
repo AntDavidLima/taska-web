1572a75cbe0569b5e017cd7b94375f1e607552c6
David Lima <antdavidlima@gmail.com>
2023-05-17 12:32:02 -0300

    chore: config base structure

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import reactLogo from "./assets/logo.svg";
+
+function App() {
+  return (
+    <>
+      <header>
+        <img src={reactLogo} alt="Taska logo" />
+      </header>
+    </>
+  );
+}
+
+export default App;
