7f0633fbdbe9f9264634069ea9d7c954c7fec756
David Lima <antdavidlima@gmail.com>
2023-05-19 09:39:12 -0300

    feat: create basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,42 @@
 import reactLogo from "./assets/logo.svg";
+import styles from "./App.module.css";
+import { PlusCircle } from "@phosphor-icons/react";
 
 function App() {
   return (
     <>
-      <header>
-        <img src={reactLogo} alt="Taska logo" />
+      <header className={styles.header}>
+        <img
+          src={reactLogo}
+          alt="Taska logo"
+          className={styles.logo}
+          width={146}
+          height={48}
+        />
       </header>
+      <main className={styles.main}>
+        <form className={styles.form}>
+          <input
+            type="text"
+            placeholder="Adicione uma nova tarefa"
+            aria-label="Nome da tarefa"
+            className={styles.input}
+          />
+          <button className={styles.submitButton}>
+            Criar <PlusCircle size={16} weight="bold" />
+          </button>
+        </form>
+        <section>
+          <div className={styles.listHeader}>
+            <p>
+              Tarefas criadas <span>0</span>
+            </p>
+            <p>
+              Concluídas <span>0</span>
+            </p>
+          </div>
+        </section>
+      </main>
     </>
   );
 }
