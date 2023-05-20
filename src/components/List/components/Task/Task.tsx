bb046a063f10dfd2676157ad814f600c2777a3b5
David Lima <antdavidlima@gmail.com>
2023-05-20 14:04:44 -0300

    feat: finish task layout

--- src/components/List/components/Task/Task.tsx
+++ src/components/List/components/Task/Task.tsx
@@ -1,14 +1,26 @@
+import { Trash } from "@phosphor-icons/react";
 import styles from "./Task.module.css";
 
 export function ListTask() {
   return (
-    <li>
-      <div className={styles.checkbox}>
-        <input type="checkbox" id="toggle-task-completion" />
-        <label htmlFor="toggle-task-completion">
-          Marcar ou desmarcar tarefa como concluída
-        </label>
-      </div>
+    <li className={styles.container}>
+      <input
+        className={styles.checkbox}
+        type="checkbox"
+        id="toggle-task-completion"
+      />
+      <label htmlFor="toggle-task-completion">
+        Marcar ou desmarcar tarefa como concluída
+      </label>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
+        repudiandae non est delectus accusantium, numquam in molestias
+        recusandae sequi ad amet saepe fugiat! Mollitia est cum sint hic debitis
+        asperiores.
+      </p>
+      <button type="button">
+        <Trash size={24} />
+      </button>
     </li>
   );
 }
